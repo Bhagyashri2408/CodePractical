7f8369350b9b4b108771529398e2994a7e1f227e
Bhagyashri2408 <udchanebv@gmail.com>
2024-12-20 15:30:00 +0530

    Test

---
+++ src/DATAPROVIDER.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class DATAPROVIDER 
+{
+	//@BeforeClass
+	@Test
+void setup()
+{
+	
+}
+void TestLogin()
+{
+	
+}
+}
---
+++ src/TestNGAnnotations.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class TestNGAnnotations 
+{
+	@Test
+public void m1()
+{
+	System.out.println("Test");
+}
+}
